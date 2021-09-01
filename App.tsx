79319f4a4f5fb3aeb718b88ab64412998b7ef011
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-09-01 12:58:23 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@ const App = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.1
+            screen and then come back to see your edits.12
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
