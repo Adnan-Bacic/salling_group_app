cc968277a95324cadc0f04326888126a9f1f46b8
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-09-01 14:55:12 +0200

    eslint setup with some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:react-hooks/recommended',
+    'plugin:react-redux/recommended',
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+        //for eslint-plugin-react-native
+        "jsx": true
+    },
+    //for eslint-config-airbnb-typescript
+    "project": './tsconfig.eslint.json'
+},
+"env": {
+  //for eslint-plugin-react-native
+  "react-native/react-native": true
+},
+//for @typescript-eslint/eslint-plugin
+"parser": "@typescript-eslint/parser",
+
 };
