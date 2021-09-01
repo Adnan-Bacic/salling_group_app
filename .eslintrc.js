b149258b3653a77a41e5b72bd1ce0a5c1b5da94c
Adnan-Bacic <adnan-bacic@hotmail.com>
2021-09-01 15:21:05 +0200

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,19 +14,32 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:react-redux/recommended',
   ],
-  "parserOptions": {
-    "ecmaFeatures": {
-        //for eslint-plugin-react-native
-        "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      // for eslint-plugin-react-native
+      jsx: true,
     },
-    //for eslint-config-airbnb-typescript
-    "project": './tsconfig.eslint.json'
-},
-"env": {
-  //for eslint-plugin-react-native
-  "react-native/react-native": true
-},
-//for @typescript-eslint/eslint-plugin
-"parser": "@typescript-eslint/parser",
+    // for eslint-config-airbnb-typescript
+    project: './tsconfig.eslint.json',
+  },
+  env: {
+  // for eslint-plugin-react-native
+    'react-native/react-native': true,
+  },
+  // for @typescript-eslint/eslint-plugin
+  parser: '@typescript-eslint/parser',
+  rules: {
+    // no need for proptypes in ts
+    'react/prop-types': [0, 'always'],
+    'react/require-default-props': [0, 'always'],
+    // in rn usually styles are at the bottom of the file
+    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
+    // error when using any kind of styled components where the name isent "Text"
+    'react-native/no-raw-text': [0, 'always'],
 
+    // syntax
+    'react/jsx-first-prop-new-line': [2, 'always'],
+    'react/jsx-max-props-per-line': [2, { maximum: 1, when: 'always' }],
+    'arrow-body-style': [2, 'always'],
+  },
 };
