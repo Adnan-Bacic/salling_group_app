bce3667f093ef1a0fca0aac39c2b39afcba367de
Adnan Bacic <adnan-bacic@hotmail.com>
2024-01-29 18:13:45 +0100

    fixed filter

--- src/screens/stores/Store.tsx
+++ src/screens/stores/Store.tsx
@@ -39,32 +39,16 @@ const Store: React.FunctionComponent<StoreInterface> = ({
     setInitialData();
   }, [currentStore.hours]);
 
-  useEffect(() => {
-    const handleChangeHourType = () => {
-      // if not null / not empty string so it doesnt run on first render
-      if (currentlyFilteredItems !== null && hoursType !== '') {
-        const filteredItemsByType = currentStore.hours.filter((item: any) => {
-          return item.type === hoursType;
-        });
-
-        setCurrentlyFilteredItems(filteredItemsByType);
-      }
-    };
-
-    handleChangeHourType();
-    // dont need currentlyFilteredItems to avoid infinite re-render
-  }, [hoursType]);
-
   const reset = () => {
     setHoursType('');
     setCurrentlyFilteredItems(currentStore.hours);
   };
 
   const showOpenOnly = () => {
-    const openOnly = currentStore.hours.filter((item: any) => {
+    const openOnly = currentStore.hours.filter((item) => {
       return item.closed === false;
     });
-    // console.log(openOnly)
+
     setCurrentlyFilteredItems(openOnly);
   };
 
@@ -128,7 +112,11 @@ const Store: React.FunctionComponent<StoreInterface> = ({
                 style={styles.chip}
                 icon={hoursType === type[0] ? 'check' : 'information'}
                 onPress={() => {
-                  setHoursType(type[0]);
+                  const filteredItemsByType = currentStore.hours.filter((item: any) => {
+                    return item.type === type[0];
+                  });
+
+                  setCurrentlyFilteredItems(filteredItemsByType);
                 }}
                 key={type[0]}
               >
